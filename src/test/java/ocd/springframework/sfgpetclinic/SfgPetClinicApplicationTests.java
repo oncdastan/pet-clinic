528aa63c0cfd1686d2546adbc0661d5135d964ea
oncdastan <oncdastan@gmail.com>
2022-12-28 01:59:08 +0300

    Initial project add

---
+++ src/test/java/ocd/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ocd.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
